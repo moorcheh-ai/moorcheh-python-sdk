1cb5c0315c12b151257a49ca7db5ed5ab0e9141d
Mahimai Raja J <mahimairaja3@gmail.com>
2025-12-05 09:50:32 -0500

    feat: added user defined types

--- moorcheh_sdk/types/__init__.py
+++ moorcheh_sdk/types/__init__.py
@@ -3,6 +3,18 @@ from typing import Any, TypeVar
 
 import httpx
 
+from .answer import AnswerResponse, ChatHistoryItem
+from .common import StatusResponse
+from .document import (
+    Document,
+    DocumentDeleteResponse,
+    DocumentGetResponse,
+    DocumentUploadResponse,
+)
+from .namespace import Namespace, NamespaceCreateResponse, NamespaceListResponse
+from .search import SearchResponse, SearchResult
+from .vector import Vector, VectorDeleteResponse, VectorUploadResponse
+
 # Common types
 JSON = dict[str, Any]
 ModelT = TypeVar("ModelT")
@@ -12,3 +24,27 @@ Timeout = float | httpx.Timeout | None
 Headers = Mapping[str, str]
 Query = Mapping[str, Any]
 Body = object
+
+__all__ = [
+    "JSON",
+    "ModelT",
+    "Timeout",
+    "Headers",
+    "Query",
+    "Body",
+    "StatusResponse",
+    "Document",
+    "DocumentUploadResponse",
+    "DocumentDeleteResponse",
+    "DocumentGetResponse",
+    "Vector",
+    "VectorUploadResponse",
+    "VectorDeleteResponse",
+    "Namespace",
+    "NamespaceCreateResponse",
+    "NamespaceListResponse",
+    "SearchResult",
+    "SearchResponse",
+    "ChatHistoryItem",
+    "AnswerResponse",
+]
