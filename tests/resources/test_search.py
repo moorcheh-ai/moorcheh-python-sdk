dc6f45c5f9603aa8dd86acb447f7cd6bf4335ff4
Mahimai Raja J <mahimairaja3@gmail.com>
2025-12-05 09:52:10 -0500

    fix: search threshold for test cases

--- tests/resources/test_search.py
+++ tests/resources/test_search.py
@@ -42,6 +42,7 @@ def test_search_success_text(client, mocker, mock_response):
             "query": query,
             "top_k": top_k,
             "kiosk_mode": False,
+            "threshold": 0.7,
         },
         params=None,
     )
